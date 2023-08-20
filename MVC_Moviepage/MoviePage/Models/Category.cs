a3b8e824c6689a22e242ba50d29c46fd31e371bc
A-Baris <barisemirdag@gmail.com>
2023-08-20 18:34:40 +0300

    crud updated changed

--- MVC_Moviepage/MoviePage/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MoviePage.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string CategoryName { get; set; }
-        public List<MovieAndCategory> MovieAndCategories { get; set; }
-    }
-}
