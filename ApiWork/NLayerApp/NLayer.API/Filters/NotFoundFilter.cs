cafdc756a280e55aaebebb13d2051a76a2989e88
A-Baris <barisemirdag@gmail.com>
2023-09-17 12:35:08 +0300

    NotFoundFilters

---
+++ ApiWork/NLayerApp/NLayer.API/Filters/NotFoundFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayer.Core.DTOs;
+using NLayer.Core.Entity;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Filters
+{
+    public class NotFoundFilter<T> : IAsyncActionFilter where T : BaseEntity
+    {
+        private readonly IService<T> _services;
+public NotFoundFilter(IService<T> services)
+        {
+            _services = services;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var idValue = context.ActionArguments.Values.FirstOrDefault();
+            if(idValue==null)
+            {
+                await next.Invoke();
+                return;
+            }
+            var id = (int)idValue;
+            var anyEntity = await _services.AnyAsync(x => x.Id == id);
+          
+            if(anyEntity)
+            {
+                await next.Invoke();
+                return;
+            }
+            context.Result=new NotFoundObjectResult(CustomResponseDto<NoContentDto>.Fail(404,$"{typeof(T).Name}({id})not found"));
+
+
+
+        }
+    }
+}
